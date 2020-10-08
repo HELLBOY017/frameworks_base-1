52215da468e126f284ccd04b86e404257eea5404
El Dainosor <eldainosor@gmail.com>
2020-10-07 23:53:52 -0400

    base: build heart shape overlay
    This is like building comic sans on the fonts package, let's have a laugh

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -68,6 +68,7 @@ LOCAL_REQUIRED_MODULES := \
         IconShapeHeartOverlay \
 	IconShapePebbleOverlay \
 	IconShapeCircleOverlay \
+	IconShapeHeartOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquareOverlay \
 	IconShapeSquircleOverlay \
